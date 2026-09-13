e8c04e42a00fefd9720f6bf81b4547e70edc2827
BadBlob <sjohdev@proton.me>
2026-09-13 14:56:34 +0200

    Added methods for counting a user-specified word

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -13,7 +13,20 @@ class TextAnalyzer(object):
         for word in set(wordList): 
             myDict[word] = wordList.count(word)
         return(myDict)
-
-# Example how the class can be used: 
+           
+    def freqOf(self, word):
+        # get frequency map
+        word = word.lower()
+        myDict = self.freqAll()
+        if (word in myDict):
+            return(myDict[word])
+        else: 
+            return 0
+            
+# User example:        
 myAnal = TextAnalyzer("One two one oNe tWo thREE")
+myDict = myAnal.freqAll()
+myWord = "Z"
+myCount = myAnal.freqOf(myWord)
 print(list(myAnal.freqAll().items()))
+print(f"\"{myWord}\" is used {myCount} times.")
